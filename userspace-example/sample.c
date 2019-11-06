7bb02b2c5e555450fab2939880797d1e9fcb062c
PalMassimo <massimo.palmisano02@gmail.com>
2019-11-06 11:18:30 +0100

    update

--- userspace-example/sample.c
+++ userspace-example/sample.c
@@ -17,9 +17,6 @@ typedef struct peer {
 } Peer;
 
 typedef struct node {
-//	unsigned int id;
-//	double lon; 			// lasciamo ma non va passato al kernel module
-//	double lat; 			// lasciamo ma non va passato al kernel module
 	unsigned int visited;
 	unsigned int num_adjacents;
 	struct peer * adjacent;
@@ -109,6 +106,7 @@ void lettore_grafo(char * file_name, int * fd_out) { // processo 1
 		perror("[lettore_grafo]: close read side of the first pipe");
 		exit(EXIT_FAILURE);
 	}
+
 	int fp = fd_out[1];
 
 //	if(dup2(first_pipe[1], STDOUT_FILENO)==-1) {
@@ -122,17 +120,25 @@ void lettore_grafo(char * file_name, int * fd_out) { // processo 1
 		exit(EXIT_FAILURE);
 	}
 
-
-	unsigned origin_id;
-	unsigned num_nodes;
-
+	unsigned int origin_id, num_nodes;
+	int catch_error=1; //default: no error
 	fscanf(inPtr,"%u %u", &num_nodes, &origin_id);
-	// TODO: controllare che sia origin_id < n
-	//fprintf(stdout,"[lettore_grafo]num_nodes = %u\n", num_nodes);
 
-	write(fp, &num_nodes, sizeof(unsigned));
-	write(fp, &origin_id, sizeof(unsigned));
+	//catch the error: origin's id>=total nodes
+	if(origin_id>=num_nodes){
+		catch_error=-1;
+		write(fp, &catch_error, sizeof(int));
+		puts("[lettore_grafo]: il numero dei nodi e' superiore all'id dell'origine ");
+		close(fp);
+		exit(EXIT_FAILURE);
+	}
+	else write(fp, &catch_error, sizeof(int));
 
+	//passing the total nodes and the origin's id
+	write(fp, &num_nodes, sizeof(unsigned int));
+	write(fp, &origin_id, sizeof(unsigned int));
+
+	//continue  to read the file text...
 	while(!feof(inPtr)){
 		unsigned node_id, peer_id;
 		unsigned int num_adj;
@@ -152,15 +158,15 @@ void lettore_grafo(char * file_name, int * fd_out) { // processo 1
 		}
 	}
 
-	//fprintf(stdout,"[lettore_grafo]finito counter=%u\n", counter);
-	fprintf(stdout, "[lettore_grafo] exit\n");
+	puts("[lettore_grafo] exit");
+
+	if(close(fd_out[1])){
+		perror("[lettore_grafo]: close write side");
+		exit(EXIT_FAILURE);
+	}
 
 	if(fclose(inPtr)){
 		perror("[lettore_grafo]: fclose");
-		exit(EXIT_FAILURE);
-	}
-	if(close(fd_out[1])){
-		perror("[lettore_grafo]: close write side");
 		exit(EXIT_FAILURE);
 	}
 }
@@ -168,34 +174,49 @@ void lettore_grafo(char * file_name, int * fd_out) { // processo 1
 void kernel_process(int fd_in, int fd_out) {
 
 	struct timespec start, stop;
-	int dowhile_counter=0;
+	int catch_error, dowhile_counter=0;
+
+	//catch eventual error
+	read(fd_in, &catch_error, sizeof(int));
+	if(catch_error!=1){
+		close(fd_in);
+		close(fd_out);
+		exit(EXIT_FAILURE);
+	}
 
 	// non usare direttamente le pipe ma usare stdin e stdout
-	unsigned origin_id, num_nodes;
-	read(fd_in, &num_nodes, sizeof(unsigned));
-	read(fd_in, &origin_id, sizeof(unsigned));
+	unsigned int origin_id, num_nodes;
+	read(fd_in, &num_nodes, sizeof(unsigned int));
+	read(fd_in, &origin_id, sizeof(unsigned int));
 
-	//fprintf(stdout, "[kernel_process] num_nodes=%u  \n", num_nodes);
+	if(num_nodes<=origin_id){
+		catch_error=-1;
+		write(fd_out, &catch_error, sizeof(int));
+		close(fd_in);
+		close(fd_out);
+		exit(EXIT_FAILURE);
+	}
 
-//	unsigned int * distances = malloc(sizeof(unsigned int)*num_nodes);
-//	Node ** previouses = malloc(sizeof(Node *) * num_nodes);
+
 	Node ** nodes = malloc(sizeof(Node *) * num_nodes);
-
-	unsigned node_id;
-	unsigned int num_adj;
 	Peer * list_adjacent;
+	unsigned int node_id, num_adj;
 
 	for(unsigned i=0; i<num_nodes; i++) {
-		read(fd_in, &node_id, sizeof(unsigned));
+		read(fd_in, &node_id, sizeof(unsigned int));
 		read(fd_in, &num_adj, sizeof(unsigned int));
 
-		// TODO: controllare che ogni sia node_id < num_nodes
-		// TODO: controllare che node_id == i
+		//check that node_id<num_nodes and that node_id==i
+		if(node_id!=i || node_id>=num_nodes){
+			close(fd_in);
+			write(fd_out, &catch_error, sizeof(int));
+			close(fd_out);
+			exit(EXIT_FAILURE);
+		}
 
 		list_adjacent=(Peer *) calloc(num_adj, sizeof(Peer));
 
 		nodes[node_id]=malloc(sizeof(Node));
-//		nodes[node_id]->id=node_id;
 		nodes[node_id]->num_adjacents=num_adj;
 		nodes[node_id]->visited=0;
 		nodes[node_id]->adjacent=list_adjacent;
@@ -207,7 +228,12 @@ void kernel_process(int fd_in, int fd_out) {
 		for(unsigned int j=0; j<num_adj; j++){
 			read(fd_in, &peer_id, sizeof(unsigned));
 			read(fd_in, &peer_distance, sizeof(unsigned));
-			// TODO: controllare che ogni id sia < num_nodes
+
+			if(peer_id>=num_nodes){
+				close(fd_in);
+				close(fd_out);
+				exit(EXIT_FAILURE);
+			}
 			list_adjacent[j].id=peer_id;
 			list_adjacent[j].distance=peer_distance;
 		}
@@ -217,14 +243,8 @@ void kernel_process(int fd_in, int fd_out) {
 
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
 
-//	Node * origin=nodes[origin_id];
-//	for(unsigned int i=0; i<num_nodes; i++){
-//		distances[i]=UINT_MAX;
-//	}
 	nodes[origin_id]->distance=0;
 	nodes[origin_id]->prev_node_id = origin_id;
-//	distances[origin_id]=0;
-//	previouses[origin_id]=nodes[origin_id];
 
 
 	unsigned unvisited=num_nodes;
@@ -243,21 +263,23 @@ void kernel_process(int fd_in, int fd_out) {
 		for(unsigned int i=0; i<nodes[indice]->num_adjacents; i++){
 			if(nodes[visit->id]->distance > nodes[indice]->distance + visit->distance){
 				nodes[visit->id]->distance = nodes[indice]->distance + visit->distance;
-				nodes[visit->id]->prev_node_id= indice;
+				nodes[visit->id]->prev_node_id = indice;
 			} visit++;
 		}
 		nodes[indice]->visited=1;
 		unvisited--;
 		dowhile_counter++;
 	}
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
+	double result = (stop.tv_sec - start.tv_sec) * 1e6 + (stop.tv_nsec - start.tv_nsec) / 1e3;
+	printf("[kernel] tempo di CPU consumato: "
+			"%lf ms, numero cicli do-while: %d\n", result, dowhile_counter);
 
-	fprintf(stdout, "[kernel_process] dijkstra finished \n");
 
 	// send results to the second process:
 	Peer * hold;
 	write(fd_out, &num_nodes, sizeof(unsigned));
 	for(unsigned i=0; i<num_nodes; i++){
-//		write(fd_out, &(i), sizeof(unsigned));
 		write(fd_out, &(nodes[i]->num_adjacents), sizeof(unsigned int));
 		hold=nodes[i]->adjacent;
 		for(unsigned int j=0; j<(nodes[i]->num_adjacents); j++){
@@ -270,19 +292,14 @@ void kernel_process(int fd_in, int fd_out) {
 	}
 
 	//free associated memory
-//	free(distances);
-//	free(previouses);
 	free(nodes);
-
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
-	double result = (stop.tv_sec - start.tv_sec) * 1e6 + (stop.tv_nsec - start.tv_nsec) / 1e3;
-	printf("[kernel] tempo di CPU consumato: "
-			"%lf ms, numero cicli do-while: %d\n", result, dowhile_counter);
 
 	if(close(fd_out)){
 		perror("[kernel]: close read side of the second pipe");
 		exit(EXIT_FAILURE);
 	}
+
+	fprintf(stdout, "[kernel_process] dijkstra finished \n");
 }
 
 void ricevi_risultati(char * file_name, int * pipe_param) {
@@ -303,14 +320,22 @@ void ricevi_risultati(char * file_name, int * pipe_param) {
 		exit(EXIT_FAILURE);
 	}
 
-	unsigned num_nodes, previous_id, distance, peer_id, peer_distance;
+	int catch_error=-1;
+	read(STDIN_FILENO, &catch_error, sizeof(int));
+	if(catch_error==-1){
+		puts("[ricevi_risultati]: kernel process checked an error");
+		close(STDIN_FILENO);
+		exit(EXIT_FAILURE);
+	}
+
+	unsigned int num_nodes, previous_id, distance, peer_id, peer_distance;
 	unsigned int num_adj;
-	read(STDIN_FILENO, &num_nodes, sizeof(unsigned));
+	read(STDIN_FILENO, &num_nodes, sizeof(unsigned int));
 
 	// TODO: se num_nodes < 0 significa che kernel process ha restituito un errore (e quindi ...)
+	// Appunto: ma num_nodes è un unsigned int
 
 	for(unsigned i=0; i<num_nodes; i++){
-//		read(STDIN_FILENO, &node_id, sizeof(unsigned));
 		read(STDIN_FILENO, &num_adj, sizeof(unsigned int));
 		fprintf(outPtr, "%u %u ", i, num_adj);
 		for(unsigned int i=0; i<num_adj; i++){
